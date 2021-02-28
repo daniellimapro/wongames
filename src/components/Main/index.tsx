ef48110b3a9e1284f7d881a08fdedfeb50e7e009
Daniel Lima <danieldeveloper1@gmail.com>
2021-02-28 19:11:35 -0300

    jest config with styled-components snapshots

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,7 +1,21 @@
+import * as S from './styles';
+
 const Main = () => (
-  <main>
-    <h1>React Avançado</h1>
-  </main>
+  <S.Wrapper>
+    <S.Title>React Avançado</S.Title>
+    <S.Logo
+      src="/img/logo.svg"
+      alt="Imagem de um átomo e React Avançado escrito ao lado."
+    />
+
+    <S.Description>
+      Typescript, ReactJS, NextJS e Styled Components
+    </S.Description>
+    <S.Illustration
+      src="/img/hero-illustration.svg"
+      alt="Ilustração de uma pessoa trabalhando em um computador."
+    />
+  </S.Wrapper>
 );
 
 export default Main;
