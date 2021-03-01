955dcd52423506f5588ee83ee7a36cd3397bbd2a
Daniel Lima <danieldeveloper1@gmail.com>
2021-03-01 13:01:17 -0300

    config manifest and pwa with next

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -14,6 +14,9 @@ function App({ Component, pageProps }: AppProps) {
           content="A simple project starter to work with TypeScript, React, NextJS and Styled-Components"
         />
         <meta name="theme-color" content="red" />
+        <link rel="shortcut icon" href="/img/icon-192.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <link rel="manifest" href="/manifest.json" />
       </Head>
       <GlobalStyles />
       <Component {...pageProps} />
